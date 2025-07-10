35a8b76bb1fb554759538960687a75e47970b13a
Rodrigo Caliman <rcaliman@gmail.com>
2025-07-09 21:42:33 -0300

    teste de remoção

--- src/test/java/br/ce/wcaquino/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/apitest/APITest.java
@@ -13,41 +13,62 @@ public class APITest {
     public static void setup() {
         RestAssured.baseURI = "http://localhost:8001/tasks-backend";
     }
-    
+
     @Test
     public void deveRetornarTarefas() {
         RestAssured.given()
-            .when()
+                .when()
                 .get("http://localhost:8001/tasks-backend/todo")
-            .then()
-                .statusCode(200)
-            ;
+                .then()
+                .statusCode(200);
 
     }
 
     @Test
     public void deveAdicionarTarefaComSucesso() {
         RestAssured.given()
-            .body("{ \"task\": \"teste via api\", \"dueDate\": \"2025-12-30\" }")
-            .contentType(ContentType.JSON)
-        .when()
-            .post("/todo")
-        .then()
-            .log().all()
-            .statusCode(201)
-        ;
+                .body("{ \"task\": \"teste via api\", \"dueDate\": \"2025-12-30\" }")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/todo")
+                .then()
+                .log().all()
+                .statusCode(201);
     }
 
     @Test
-     public void deveAdicionarTarefaInvalida() {
-         RestAssured.given()
-             .body("{ \"task\": \"teste via api\", \"dueDate\": \"2022-12-30\" }")
-             .contentType(ContentType.JSON)
-         .when()
-             .post("/todo")
-         .then()
-             .log().all()
-             .statusCode(400)
-             .body("message", CoreMatchers.is("Due date must not be in past"));   
+    public void deveAdicionarTarefaInvalida() {
+        RestAssured.given()
+                .body("{ \"task\": \"teste via api\", \"dueDate\": \"2022-12-30\" }")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/todo")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("message", CoreMatchers.is("Due date must not be in past"));
+    }
+
+    @Test
+    public void deveRemoverTarefaComSucesso() {
+        // inserir
+        Integer id = RestAssured.given()
+                .body("{ \"task\": \"Tarefa teste\", \"dueDate\": \"2025-12-30\" }")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/todo")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract().path("id");
+        System.out.println(id);
+
+        // remover
+        RestAssured.given()
+        .when()
+        .delete("/todo/" + id)
+        .then()
+        .statusCode(204)
+        ;
     }
 }
